e798c709a7a528c85de9e1bc944b99727350d9eb
Maths79 <maths79.mint@gmail.com>
2024-09-10 18:06:01 +0800

    prime: up to 17, maybe due to recursive call

--- user/primes.c
+++ user/primes.c
@@ -2,56 +2,84 @@
 #include "kernel/fcntl.h"
 #include "user/user.h"
 
+#define MAX_NUM 100
+
+void primeproc(int) __attribute__((noreturn));
+
 void
 primeproc(int readDp)
 {
   int prime;
   if(read(readDp, &prime, 4) == 0){
     close(readDp);
-    return;
+    exit(0);
   }
-  fprintf(1, "prime %d\n", prime);
+  printf("prime %d\n", prime);
 
   int right[2];
   if(pipe(right) < 0){
-    fprintf(1, "pipe error\n");
+    fprintf(2, "pipe error\n");
     exit(1);
   }
 
-  if(fork() == 0){
+  int n[MAX_NUM];
+  for(int i = 0; i< MAX_NUM; i++)
+    n[i] = 1;
+  int s = 0;
+  while(read(readDp, &n[s], 4) > 0)
+    s++;
+  close(readDp);
+
+  int pid = fork();
+
+  if(pid < 0){
+    fprintf(2, "fork error\n");
+    exit(1);
+  }
+
+  if(pid == 0){
     close(right[1]);
     primeproc(right[0]);
-    close(right[0]);
-    exit(0);
   } else {
     close(right[0]);
-    int n;
-    while(read(readDp, &n, 4)){
-      if(n % prime != 0){
-        write(right[1], &n, 4);
-      }
+    for(int i = 0; n[i] != 1; i++){
+      if(n[i] % prime != 0)
+        write(right[1], &n[i], 4);
     }
     close(right[1]);
     wait(0);
   }
+  exit(0);
 }
 
 int
 main(int argc, char *argv[])
 {
   int p[2];
-  pipe(p);
+  if(pipe(p) < 0){
+    fprintf(2, "pipe error\n");
+    exit(1);
+  }
 
-  if(fork() == 0){
+  int pid = fork();
+
+  if(pid < 0){
+    fprintf(2, "fork error\n");
+    exit(1);
+  }
+
+  if(pid == 0){
     close(p[1]);
     primeproc(p[0]);
-    close(p[0]);
-    exit(0);
   } else {
     close(p[0]);
-    for(int i = 0; i < 276; i++){
-      int n = i + 2;
-      write(p[1], &n, 4);
+    int n[MAX_NUM];
+    for(int i = 0; i < MAX_NUM; i++){
+      n[i] = i + 2;
+      if(write(p[1], &n[i], 4) < 0){
+        fprintf(2, "write error\n");
+        exit(1);
+      }
     }
     close(p[1]);
     wait(0);
